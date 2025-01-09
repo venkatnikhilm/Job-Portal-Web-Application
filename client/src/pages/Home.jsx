62f79043173abe7c9f66a6e07d57d12d916f13ec
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-10 00:13:01 +0530

    Added Hero Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
         <NavBar />
+        <Hero />
     </div>
   )
 }
