49ad36e85c4f79d0da52476a50cf2c5c2e43be40
Venkat Nikhil M <venkatnikhilm@Venkats-MacBook-Air.local>
2025-01-10 01:35:37 +0530

    Added search logic in hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Hero = () => {
+    
+    const {setSearchFilter, setIsSearched}=useContext(AppContext)
+    
+    const titleRef = useRef(null)
+    const locationRef = useRef(null)
+
+    const onSearch = () => {
+        setSearchFilter({
+            title: titleRef.current.value,
+            location: locationRef.current.value,
+        })
+        setIsSearched(true)
+    }
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
         <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
@@ -12,15 +26,17 @@ const Hero = () => {
                     <img className='h-4 sm:h-5' src={assets.search_icon} alt='search icon' />
                     <input type="text" 
                     placeholder='Search for jobs'
+                    ref={titleRef}
                     className='max-sm:text-xs p-2 rounded outline-none w-full'/>
                 </div>
                 <div className='flex items-center'>
                     <img className='h-4 sm:h-5' src={assets.location_icon} alt='location icon' />
                     <input type="text" 
                     placeholder='Location'
+                    ref={locationRef}
                     className='max-sm:text-xs p-2 rounded outline-none w-full'/>
                 </div>
-                <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
+                <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
             </div>
         </div>
         <div className='border border-gray-600 shadow-md  mx-2 mt-5 p-6 flex rounded-md'>
