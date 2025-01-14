2a8d5f19d8c76786d6755335adba7614dcfd6558
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-14 20:08:59 +0530

    Added job card logic to navigate to application page

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const JobCard = ({ job }) => {
+
+  const navigate = useNavigate()
   return (
     <div className='border p-6 shadow rounded'>
         <div className='flex justify-between items-center'>
@@ -14,8 +17,8 @@ const JobCard = ({ job }) => {
         </div>
         <p className="text-gray-500 text-sm mt-4" dangerouslySetInnerHTML={{__html: job.description.slice(0, 150)}}></p>
             <div className='mt-4 flex gap-4 text-sm'>
-                <button className='bg-blue-600 text-white rounded px-4 py-2'>Apply Now</button>
-                <button className= 'text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn More</button>
+                <button onClick={()=>{navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='bg-blue-600 text-white rounded px-4 py-2'>Apply Now</button>
+                <button onClick={()=>{navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className= 'text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn More</button>
             </div>
     </div>
   )
