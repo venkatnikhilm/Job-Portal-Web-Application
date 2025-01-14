2e4d56a9ba32a75315e8ff36976aed2a9eacdff3
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-15 01:23:21 +0530

    Partially completed ApplyJob.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
 
     const {openSignIn} = useClerk()
     const {user} = useUser()
+    const navigate = useNavigate()
 
   return (
     <div className='shadow py-4'>
       <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-        <img src = {assets.logo} alt = 'logo' />
+        <img onClick={() => navigate('/')} className='cursor-pointer' src = {assets.logo} alt = 'logo' />
         {
             user 
             ?<div className='flex items-center gap-3'>
                 <Link to = '/appications'>Applied jobs</Link>
                 <p>|</p>
-                <p max-sm:hidden>Hi, {user.firstName + " "+user.lastName}</p>
+                <p className='max-sm:hidden'>Hi, {user.firstName + " "+user.lastName}</p>
             </div>
             :<div className='flex gap-4 max-sm:text-small'>
             <button className='text-gray-600'>Recruiter Login</button>
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import Loading from '../components/Loading'
+import NavBar from '../components/NavBar'
+import { assets } from '../assets/assets'
+import kconvert from 'k-convert'
+import moment from 'moment'
+import JobCard from '../components/JobCard'
 
 const ApplyJob = () => {
-  return (
-    <div>
-      <h1>Apply jobs</h1>
+  const {id} = useParams()
+  const [jobData ,setJobData] = useState(null)
+  const {jobs} = useContext(AppContext)
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id === id)
+    if (data.length !== 0) {
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+  useEffect(() => {
+    if (jobs.length !== 0){
+      fetchJob()
+    }
+  }, [id,jobs])
+  return jobData ? (
+    <>
+    <NavBar />
+    <div className=' min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto'>
+      <div className='bg-white text-black rounded-lg w-ful'>
+        <div className='flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl'>
+          <div className='flex flex-col md:flex-row items-center'>
+            <img className='h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border ' src={jobData.companyId.image} alt="company" />
+            <div className='text-centermd:text-left text-neutral-700'>
+              <h1 className='text-2xl sm:text-4xl font-medium'>{jobData.title}</h1>
+              <div className='flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2'>
+                <span className='flex items-center gap-1'>
+                  <img src={assets.suitcase_icon}/>
+                  {jobData.companyId.name}
+                </span>
+                <span className='flex items-center gap-1'>
+                  <img src={assets.location_icon}/>
+                  {jobData.location}
+                </span>
+                <span className='flex items-center gap-1'>
+                  <img src={assets.person_icon}/>
+                  {jobData.level}
+                </span>
+                <span className='flex items-center gap-1'>
+                  <img src={assets.money_icon}/>
+                  CTC : {kconvert.convertTo(jobData.salary)}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
+            <button className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
+            <p className='mt-1 text-gray-600' >Posted {moment(jobData.date).fromNow()}</p>
+          </div>
+
+        </div>
+      <div className='flex flex-col lg:flex-row justify-between items-start'>
+        <div className='w-full lg:w-2/3'>
+          <h2 className='font-bold mb-4 text-2xl'>Job Description</h2>
+          <div className='rich-text' dangerouslySetInnerHTML={{__html: jobData.description}}></div>
+          <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
+        </div>
+        {/* Right Section More Jobs
+        <h2>More jobs from {jobData.companyId.name}</h2>
+          {jobs.filter(job => job._id !== jobData._id && jobs.companyId._id === jobData.companyId._id)
+          .filter(job => true).slice(0,4).map(job,index => <JobCard key={index} job={job}/>)} */}
+      </div>
+      </div>
     </div>
+    </>
+  ):(
+    <Loading />
   )
 }
 
