f169a0404174bddac836241184f9497b042157cf
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-15 16:24:09 +0530

    Added Applications.jsx page

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const Applications = () => {
   return (
-    <div>
-      <h1>Applications</h1>
-    </div>
+    <>
+    <NavBar />
+    </>
   )
 }
 
