1293aca7bea2d44d86456cde3eb07c98a9646fb6
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-16 18:59:49 +0530

    Added NavBar to applications page

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 
 const Applications = () => {
+  const [isEdit, setIsEdit] = useState(false)
   return (
     <>
     <NavBar />
