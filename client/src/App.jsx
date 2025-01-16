3e9720596bbc80d67f585031d55632804e232312
Venkat Nikhil M <venkatnikhil4699@gmail.com>
2025-01-17 00:05:18 +0530

    Added Routes to recruiter pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import Applications from './pages/Applications'
 import ApplyJob from './pages/ApplyJob'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
 const App = () => {
 
   const {showRecruiterLogin} = useContext(AppContext)
@@ -16,6 +20,11 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/applications' element={<Applications />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='manage-job' element={<ManageJobs />} />
+          <Route path='view-applications' element={<ViewApplications />} />
+        </Route>
       </Routes>
     
     </div>
